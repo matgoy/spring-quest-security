d46602014a5aa40dddf28fc85cc7fafd1258d9a6
matgoy <gohiermathieu78@gmail.com>
2022-11-07 12:39:29 +0100

    correctif

--- src/main/java/com/wws/shield/config/MySecurityConfig.java
+++ src/main/java/com/wws/shield/config/MySecurityConfig.java
@@ -2,6 +2,7 @@ package com.wws.shield.config;
 
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -30,5 +31,16 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter{
 
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/avengers/assemble").access("hasRole('CHAMPION')")
+                .antMatchers("/base-secrets").hasRole("DIRECTOR")
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
 
 }
